1cbf669a02955a08e43419e29c1f1e280113bd28
Müller Neo <Neo.Mueller@swisscom.com>
2023-05-08 15:15:00 +0200

    feat: do the TODO'S

--- src/regex.test.py
+++ src/regex.test.py
@@ -8,18 +8,14 @@ TypeDomain = input("Gib die Domain deiner E-Mail-Adresse ein (z.B. gmail.com): "
 SecondDomain = input("Möchten Sie eine weitere Domain hinzufügen? (ja/nein): ")
 
 # Add second domain if user input is 'ja'
-if SecondDomain == "ja":
+if SecondDomain.lower() == "ja":
     TypeDomain2 = input("Bitte geben Sie die zweite Domain ein: ")
 
 # Input emails
 TypeMoreEmails = input("Gib deine Email(s) ein, getrennt durch ein Komma: ")
-#TODO Does it work when you write it in uppercase?
-#TODO Can I only type one Email?
 TypeMoreEmails = TypeMoreEmails.split(",")
 
 # Check each email for duplicates and add to list
-
-#TODO Does this actually work?
 for email in TypeMoreEmails:
     email = email.strip()
     if re.match(r"[^@]+@[^@]+\.[^@]+", email):
@@ -31,11 +27,12 @@ for email in TypeMoreEmails:
         print(f'Die E-Mail-Adresse "{email}" ist ungültig.')
 
 # Check if the emails have the desired domain(s)
-# TODO Should we change this?
 for email in Emails:
     domain_regex = r"@(.+)$"
     match = re.search(domain_regex, email)
     if match:
         domain = match.group(1)
-        if domain == TypeDomain or domain == TypeDomain2:
+        if domain == TypeDomain or (SecondDomain.lower() == "ja" and domain == TypeDomain2):
             print(f'Die E-Mail-Adresse "{email}" hat eine der gewünschten Domains.')
+        else:
+            print(f'Die E-Mail-Adresse "{email}" hat nicht die gewünschte Domain.')
