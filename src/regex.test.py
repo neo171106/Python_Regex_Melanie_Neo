8c115ef473d11f9892a27dfcc5fdfe4ad2a61851
Müller Neo <Neo.Mueller@swisscom.com>
2023-05-08 13:30:41 +0200

    fix: shorted the code and you can add now multiple emails

--- src/regex.test.py
+++ src/regex.test.py
@@ -1,35 +1,34 @@
 import re
 
+# Initial list of emails
 Emails = ["Melanie.burri@gmail.com", "neo.mueller@gmx.ch", "KlaudiaEngel@einrot.com"]
 
-# Eingabeaufforderung für die E-Mail-Adresse und die Domain
-TypeEmail = str(input("Gib deine Email ein: "))
-TypeDomain = str(input('Gib die Domain deiner E-Mail-Adresse ein (z.B. gmail.com): '))
-SecondDomain = str(input('Möchten Sie eine weitere Domain hinzufügen? (ja/nein): '))
+# Input email domain and second domain (optional)
+TypeDomain = input("Gib die Domain deiner E-Mail-Adresse ein (z.B. gmail.com): ")
+SecondDomain = input("Möchten Sie eine weitere Domain hinzufügen? (ja/nein): ")
+TypeDomain2 = ""
 
-# Trennen von Benutzername und Domäne
-username, domain = TypeEmail.split('@')
+# Add second domain if user input is 'ja'
+if SecondDomain == "ja":
+    TypeDomain2 = input("Bitte geben Sie die zweite Domain ein: ")
 
-# Überprüfen, ob die eingegebene E-Mail-Adresse die gewünschte Domain enthält
-if domain == TypeDomain:
-    print('Die eingegebene E-Mail hat die gleiche Domäne wie der eingegebene Typ.')
+# Input emails
+TypeMoreEmails = input("Gib deine Email(s) ein, getrennt durch ein Komma: ")
+TypeMoreEmails = TypeMoreEmails.split(",")
 
-# Überprüfen, ob eine zweite Domain hinzugefügt werden soll
-if SecondDomain == 'ja':
-    TypeDomain2 = str(input('Bitte geben Sie die zweite Domain ein: '))
+# Add the new emails to the list
+for email in TypeMoreEmails:
+    Emails.append(email.strip())
 
-    for email in Emails:
-        domain_regex = r"@(.+)$"
-        match = re.search(domain_regex, email)
-        if match:
-            domain = match.group(1)
-            if domain == TypeDomain or domain == TypeDomain2:
-                print(f'Die E-Mail-Adresse {email} hat eine der gewünschten Domains.')
-else:
-    for email in Emails:
-        domain_regex = r"@(.+)$"
-        match = re.search(domain_regex, email)
-        if match:
-            domain = match.group(1)
-            if domain == TypeDomain:
-                print(f'Die E-Mail-Adresse {email} hat die gewünschte Domain.')
+# Check if all the emails are unique
+if len(Emails) != len(set(Emails)):
+    print("Es gibt mindestens eine E-Mail-Adresse, die bereits verwendet wurde.")
+
+# Check if the emails have the desired domain(s)
+for email in Emails:
+    domain_regex = r"@(.+)$"
+    match = re.search(domain_regex, email)
+    if match:
+        domain = match.group(1)
+        if domain == TypeDomain or domain == TypeDomain2:
+            print(f'Die E-Mail-Adresse {email} hat eine der gewünschten Domains.')
