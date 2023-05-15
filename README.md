1f4bfe907a24ae9dbc1edc9d5105e72f98a1d3ed
Müller Neo <Neo.Mueller@swisscom.com>
2023-05-15 14:24:51 +0200

    feat: add txt file as output

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Python_Regex_Melanie_Neo
+
+Erstelle eine Liste mit name, vorname, email
+-Text Datei einlesen -> Erstellt liste von vorname und nachname
+-Email sortieren checken ob sie doppelt sind mit regex und lowercase nach domains und diese abzählen
+-Namens Liste sortiert nach buchstaben
+
+Ziel: Welcher Anbieter wird am meisten genutzt?
--- src/regex.test.py
+++ src/regex.test.py
@@ -1,44 +1,75 @@
 import re
 
-# Create empty list to store emails
+# Erstelle leere Listen, um Namen und E-Mails zu speichern
+Namen = []
 Emails = []
 
-# Input email domain and second domain (optional)
-TypeDomain = input("Gib die Domain deiner E-Mail-Adresse ein (z.B. gmail.com): ")
-SecondDomain = input("Möchten Sie eine weitere Domain hinzufügen? (ja/nein): ")
+# Eingabe gib deinen Vornamen ein. Weiter Namen getrennt durch ein Komma
+TypeNamen = input("Gib deine Namen ein: ")
 
-# Add second domain if user input is 'ja'
-if SecondDomain == "ja":
-    TypeDomain2 = input("Bitte geben Sie die zweite Domain ein: ")
-
-# Input emails
-TypeMoreEmails = input("Gib deine Email(s) ein, getrennt durch ein Komma: ")
-TypeMoreEmails = TypeMoreEmails.split(",")
-
-# Check each email for duplicates and add to list
-for email in TypeMoreEmails:
-    email = email.strip()
-    if re.match(r"^([a-zA-Z0-9_.+-]+)@([a-zA-Z0-9-]+\.)+([a-zA-Z0-9]{2,})$", email):
-        if email in Emails:
-            print(f'Die E-Mail-Adresse "{email}" wurde bereits hinzugefügt.')
-        else:
-            Emails.append(email)
+# Füge Namen zur Liste hinzu
+for name in TypeNamen.split(","):
+    name = name.strip()
+    if name:
+        Namen.append(name)
     else:
-        print(f'Die E-Mail-Adresse "{email}" ist ungültig.')
+        print("Ungültiger Name.")
 
-        #Sort the Email alphabetic
-        Emails = sorted(Emails, key=str.lower)
+# Input E-Mail-Adresse(n)
+while True:
+    # Eingabe der E-Mail-Adresse
+    TypeEmails = input("Gib deine E-Mail-Adresse(n) ein, getrennt durch ein Komma: ")
+    TypeEmails = TypeEmails.split(",")
 
-# Check if the emails have the desired domain(s)
-for email in Emails:
-    domain_regex = r"@(.+)$"
-    match = re.search(domain_regex, email)
-    if match:
-        domain = match.group(1)
-        if domain == TypeDomain or domain == TypeDomain2:
-            print(f'Die E-Mail-Adresse "{email}" hat eine der gewünschten Domains.')
+    # Überprüfe jede E-Mail-Adresse auf Duplikate und füge sie zur Liste hinzu
+    for email in TypeEmails:
+        email = email.strip()
+        if re.match(r"^([a-zA-Z0-9_.+-]+)@([a-zA-Z0-9-]+\.)+([a-zA-Z0-9]{2,})$", email):
+            if email in Emails:
+                print(f'Die E-Mail-Adresse "{email}" wurde bereits hinzugefügt.')
+            else:
+                Emails.append(email)
+        else:
+            print(f'Die E-Mail-Adresse "{email}" ist ungültig.')
 
-# Print all emails
-print("Alle eingegebenen E-Mails:")
-for email in Emails:
-    print(email)
+    # Überprüfe, ob weitere E-Mails eingegeben werden sollen
+    while True:
+        TypeMoreEmails = input("Möchten Sie weitere E-Mail-Adressen hinzufügen? (ja/nein): ")
+        if TypeMoreEmails.lower() == "ja":
+            break
+        elif TypeMoreEmails.lower() == "nein":
+            # Sortiere die E-Mails alphabetisch
+            Emails = sorted(Emails, key=str.lower)
+
+            # Überprüfe, ob die E-Mails die gewünschten Domains haben
+            Domains = {}
+            for email in Emails:
+                domain_regex = r"@(.+)$"
+                match = re.search(domain_regex, email)
+                if match:
+                    domain = match.group(1)
+                    if domain not in Domains:
+                        Domains[domain] = 1
+                    else:
+                        Domains[domain] += 1
+                    print(f'Die E-Mail-Adresse "{email}" hat die Domain "{domain}".')
+
+            # Schreibe Anzahl der Domains in eine Textdatei
+            with open("domains.txt", "a") as f:
+                f.write("\nAnzahl der Domains:\n")
+                for domain, count in Domains.items():
+                    f.write(f"{domain}: {count}\n")
+
+            # Gib alle Namen und E-Mails aus
+            print("Alle eingegebenen Namen:")
+            for name in Namen:
+                print(name)
+            print("Alle eingegebenen E-Mail-Adressen:")
+            for email in Emails:
+                print(email)
+
+            # Beende das Programm
+            exit()
+        else:
+            print("Ungültige Eingabe. Bitte geben Sie 'ja' oder 'nein' ein.")
+
